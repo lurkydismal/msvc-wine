aec41f09b65f7ac8590e9940c46cdd021d97390f
LurkyDismal <lurkydismal@duck.com>
2026-07-07 19:20:40 +0000

    style(msvctricks): format

--- msvctricks.cpp
+++ msvctricks.cpp
@@ -15,24 +15,22 @@
  */
 
 #include <windows.h>
+
 #include <shlwapi.h>
 
+#pragma comment( lib, "shell32.lib" )
+#pragma comment( lib, "shlwapi.lib" )
+#pragma comment( linker, "/ENTRY:wWinMainCRTStartup" )
+#pragma comment( linker, "/SUBSYSTEM:CONSOLE" )
 
-#pragma comment(lib, "shell32.lib")
-#pragma comment(lib, "shlwapi.lib")
-#pragma comment(linker, "/ENTRY:wWinMainCRTStartup")
-#pragma comment(linker, "/SUBSYSTEM:CONSOLE")
-
-
-static HANDLE hStdIn  = INVALID_HANDLE_VALUE;
+static HANDLE hStdIn = INVALID_HANDLE_VALUE;
 static HANDLE hStdOut = INVALID_HANDLE_VALUE;
 static HANDLE hStdErr = INVALID_HANDLE_VALUE;
 static HANDLE hChildJob;
 
-static DWORD run(LPWSTR lpCmdLine)
-{
+static DWORD run( LPWSTR lpCmdLine ) {
     STARTUPINFOW si = {};
-    si.cb = sizeof(si);
+    si.cb = sizeof( si );
     si.dwFlags = STARTF_USESTDHANDLES;
     si.hStdInput = hStdIn;
     si.hStdOutput = hStdOut;
@@ -41,107 +39,106 @@ static DWORD run(LPWSTR lpCmdLine)
     PROCESS_INFORMATION pi = {};
 
     DWORD dwExitCode;
-    if (CreateProcessW(nullptr, lpCmdLine, nullptr, nullptr, TRUE, 0, nullptr, nullptr, &si, &pi))
-    {
-        if (hChildJob)
-        {
-            AssignProcessToJobObject(hChildJob, pi.hProcess);
+    if ( CreateProcessW( nullptr, lpCmdLine, nullptr, nullptr, TRUE, 0, nullptr,
+                         nullptr, &si, &pi ) ) {
+        if ( hChildJob ) {
+            AssignProcessToJobObject( hChildJob, pi.hProcess );
         }
 
-        WaitForSingleObject(pi.hProcess, INFINITE);
+        WaitForSingleObject( pi.hProcess, INFINITE );
 
-        if (!GetExitCodeProcess(pi.hProcess, &dwExitCode))
-        {
+        if ( !GetExitCodeProcess( pi.hProcess, &dwExitCode ) ) {
             dwExitCode = GetLastError();
         }
 
-        CloseHandle(pi.hProcess);
-        CloseHandle(pi.hThread);
-    }
-    else
-    {
+        CloseHandle( pi.hProcess );
+        CloseHandle( pi.hThread );
+    } else {
         dwExitCode = GetLastError();
     }
 
     return dwExitCode;
 }
 
-static DWORD mt(LPWSTR lpCmdLine)
-{
-    DWORD dwExitCode = run(lpCmdLine);
+static DWORD mt( LPWSTR lpCmdLine ) {
+    DWORD dwExitCode = run( lpCmdLine );
     // https://gitlab.kitware.com/cmake/cmake/-/blob/v3.26.0/Source/cmcmd.cxx#L2405
-    if (dwExitCode == 0x41020001)
+    if ( dwExitCode == 0x41020001 )
         dwExitCode = 0xbb;
     return dwExitCode;
 }
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
-{
-    (void) hInstance;
-    (void) hPrevInstance;
-    (void) nCmdShow;
+int WINAPI wWinMain( HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPWSTR lpCmdLine,
+                     int nCmdShow ) {
+    ( void )hInstance;
+    ( void )hPrevInstance;
+    ( void )nCmdShow;
 
     int argc = 0;
-    wchar_t** argv = CommandLineToArgvW(lpCmdLine, &argc);
-    if (argc <= 0) return 0;
+    wchar_t** argv = CommandLineToArgvW( lpCmdLine, &argc );
+    if ( argc <= 0 )
+        return 0;
 
-    wchar_t buf[32768];
-    if (GetEnvironmentVariableW(L"WINE_MSVC_STDIN", buf, ARRAYSIZE(buf)))
-    {
+    wchar_t buf[ 32768 ];
+    if ( GetEnvironmentVariableW( L"WINE_MSVC_STDIN", buf,
+                                  ARRAYSIZE( buf ) ) ) {
         SECURITY_ATTRIBUTES attr = {};
-        attr.nLength = sizeof(attr);
+        attr.nLength = sizeof( attr );
         attr.bInheritHandle = TRUE;
 
-        hStdIn = CreateFileW(buf, GENERIC_READ,
-            FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
-            &attr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
+        hStdIn =
+            CreateFileW( buf, GENERIC_READ,
+                         FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
+                         &attr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr );
     }
-    if (GetEnvironmentVariableW(L"WINE_MSVC_STDOUT", buf, ARRAYSIZE(buf)))
-    {
+    if ( GetEnvironmentVariableW( L"WINE_MSVC_STDOUT", buf,
+                                  ARRAYSIZE( buf ) ) ) {
         SECURITY_ATTRIBUTES attr = {};
-        attr.nLength = sizeof(attr);
+        attr.nLength = sizeof( attr );
         attr.bInheritHandle = TRUE;
 
-        hStdOut = CreateFileW(buf, GENERIC_WRITE,
-            FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
-            &attr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
+        hStdOut =
+            CreateFileW( buf, GENERIC_WRITE,
+                         FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
+                         &attr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr );
     }
-    if (GetEnvironmentVariableW(L"WINE_MSVC_STDERR", buf, ARRAYSIZE(buf)))
-    {
+    if ( GetEnvironmentVariableW( L"WINE_MSVC_STDERR", buf,
+                                  ARRAYSIZE( buf ) ) ) {
         SECURITY_ATTRIBUTES attr = {};
-        attr.nLength = sizeof(attr);
+        attr.nLength = sizeof( attr );
         attr.bInheritHandle = TRUE;
 
-        hStdErr = CreateFileW(buf, GENERIC_WRITE,
-            FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
-            &attr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
+        hStdErr =
+            CreateFileW( buf, GENERIC_WRITE,
+                         FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
+                         &attr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr );
     }
 
-    if (hStdIn == INVALID_HANDLE_VALUE)
-    {
-        hStdIn = GetStdHandle(STD_INPUT_HANDLE);
+    if ( hStdIn == INVALID_HANDLE_VALUE ) {
+        hStdIn = GetStdHandle( STD_INPUT_HANDLE );
     }
-    if (hStdOut == INVALID_HANDLE_VALUE)
-    {
-        hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    if ( hStdOut == INVALID_HANDLE_VALUE ) {
+        hStdOut = GetStdHandle( STD_OUTPUT_HANDLE );
     }
-    if (hStdErr == INVALID_HANDLE_VALUE)
-    {
-        hStdErr = GetStdHandle(STD_ERROR_HANDLE);
+    if ( hStdErr == INVALID_HANDLE_VALUE ) {
+        hStdErr = GetStdHandle( STD_ERROR_HANDLE );
     }
 
-    if (hChildJob = CreateJobObjectW(nullptr, nullptr))
-    {
+    if ( hChildJob = CreateJobObjectW( nullptr, nullptr ) ) {
         JOBOBJECT_EXTENDED_LIMIT_INFORMATION info = {};
-        info.BasicLimitInformation.LimitFlags = JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE
-                                              | JOB_OBJECT_LIMIT_SILENT_BREAKAWAY_OK;
-        SetInformationJobObject(hChildJob, JobObjectExtendedLimitInformation, &info, sizeof(info));
+        info.BasicLimitInformation.LimitFlags =
+            JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE |
+            JOB_OBJECT_LIMIT_SILENT_BREAKAWAY_OK;
+        SetInformationJobObject( hChildJob, JobObjectExtendedLimitInformation,
+                                 &info, sizeof( info ) );
     }
 
-    LPCWSTR const exe = PathFindFileNameW(argv[0]);
+    LPCWSTR const exe = PathFindFileNameW( argv[ 0 ] );
 
-    if (PathMatchSpecW(exe, L"mt.exe"))
-        return mt(lpCmdLine);
+    if ( PathMatchSpecW( exe, L"mt.exe" ) )
+        return mt( lpCmdLine );
 
-    return run(lpCmdLine);
+    return run( lpCmdLine );
 }
